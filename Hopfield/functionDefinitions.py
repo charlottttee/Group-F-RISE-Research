cb4100478d1e7f6f1f46825131aedf8264e0cc2f
charlottttee <68303075+charlottttee@users.noreply.github.com>
2020-08-07 13:18:05 -0700

    created measurement functions

--- Hopfield/functionDefinitions.py
+++ Hopfield/functionDefinitions.py
@@ -17,19 +17,17 @@ from mnistutils import load_mnist
 from utils import *
 from whitening import *
 
+def prune (net, density=0.02, whiten=False):
+    weights = net.weights.flatten()
+    K = len(weights)
+    ixs = np.random.choice(K, round(K * (1 - density)))
+    weights[ixs] = 0
+    weights = weights.reshape(net._N, net._N)
+    if whiten:
+        weights *= net._N * 2
+    net._weights = weights
 
-def simulate(net, pattern = "random", 
-             pauseVal=0.2, 
-             numPatterns=5, 
-             patternIndex=0, 
-             whitenVal=False, 
-             plotVal=False, 
-             tauVal=0.1, 
-             itVal=50):
-    
-    # Setup logging and some constants.
-    logging.basicConfig(level=logging.INFO)
-
+def loadPatterns (net, numPatterns, patternIndex, whitenVal, pattern='random'):
     # Create the network, and specify parameters.
     n_patterns = numPatterns
     ind_state_0 = patternIndex
@@ -47,7 +45,27 @@ def simulate(net, pattern = "random",
     tform.fit(P)
     P = tform(P)
     net.store_patterns(P)
+    
+    return P0, ind_state_0, tform
 
+def simulate(net, pattern = "random", 
+             pauseVal=0.2, 
+             numPatterns=5, 
+             patternIndex=0, 
+             whitenVal=False, 
+             plotVal=False, 
+             tauVal=0.1, 
+             itVal=50,
+             density=0.02):
+    
+    # Setup logging and some constants.
+    logging.basicConfig(level=logging.INFO)
+
+    # Create the network, and specify parameters.
+    ind_state_0 = patternIndex
+    
+    P0, ind_state_0, tform = loadPatterns(net, numPatterns, patternIndex, whitenVal, pattern=pattern)
+    prune(net, density=density, whiten=whitenVal)
 
     # Initialize state vector.
     state_0 = P0[ind_state_0]
@@ -149,16 +167,31 @@ def calcPerf(templateMemory,
     if plotVal:    
         plt.figure()
         plt.imshow(plotDifference, cmap = 'gray', vmin = 0, vmax = 2)
+    
+def measureWeights(net):
+    numConnections = 0
+    for x in range (784):
+        for y in range (784):
+            if net.weights[x,y] != 0:
+                numConnections += 1
+    return numConnections
 
-def prune (net, density=0.02, whiten=False):
-    weights = net.weights.flatten()
-    K = len(weights)
-    ixs = np.random.choice(K, round(K * (1 - density)))
-    weights[ixs] = 0
-    weights = weights.reshape(net._N, net._N)
-    if whiten:
-        weights *= net._N * 2
-    net._weights = weights
+def measureAverage(net):
+    totalConnectionVal = 0
+    for x in range (784):
+        for y in range (784):
+            totalConnectionVal += abs(net.weights[x,y])
+    return totalConnectionVal/(784*784)
+
+def measureAverageOfExistent(net):
+    totalConnectionVal = 0
+    numAlive = 0
+    for x in range (784):
+        for y in range (784):
+            totalConnectionVal += abs(net.weights[x,y])
+            if net.weights[x,y] != 0:
+                numAlive += 1
+    return totalConnectionVal/numAlive
 
 def TBI (net, cod):
     array = np.zeros((1, 784))
@@ -167,5 +200,6 @@ def TBI (net, cod):
         if chance <= cod:
             array[0, i] = 1
             net.weights[:, i] = 0
+            net.weights[i, :] = 0
     array = array.reshape(28, 28)
     return array
--- Hopfield/simulation.py
+++ Hopfield/simulation.py
@@ -8,7 +8,9 @@ Created on Wed Aug  5 11:40:27 2020
 
 
 from continuous_network import ContinuousHopfieldNetwork
-from functionDefinitions import calcPerf, simulate, prune, TBI
+import numpy as np
+from functionDefinitions import calcPerf, simulate, prune, TBI, measureAverage, measureAverageOfExistent
+from functionDefinitions import measureWeights
 
 """import time
 seed = round(time.time())
@@ -18,28 +20,47 @@ n_rows, n_cols = 28, 28
 N = n_rows * n_cols
 network1 = ContinuousHopfieldNetwork(N)
 
-prune(network1)
-
 a, b = simulate(net=network1, 
              pauseVal=0, 
              numPatterns=5, 
              patternIndex=2, 
              whitenVal=False, 
-             plotVal=True, 
+             plotVal=False, 
              tauVal=0.1, 
-             itVal=50)
+             itVal=50,
+             density=0.01)
 
-calcPerf(a, b, 28, 28, plotVal=True)
+calcPerf(a, b, 28, 28, plotVal=False)
+
+beforeDensity = measureWeights(network1)/(784*784)
 
 array1 = TBI(network1, 0.1)
+aliveNumber = 784 - np.sum(array1)
+aliveWeights = aliveNumber ** 2
 
 a, b = simulate(net=network1, 
              pauseVal=0, 
              numPatterns=5, 
              patternIndex=2, 
              whitenVal=False, 
-             plotVal=True, 
+             plotVal=False, 
              tauVal=0.1, 
              itVal=50)
 
-calcPerf(a, b, 28, 28, plotVal=True, arrayVal = True, array = array1)
+calcPerf(a, b, 28, 28, plotVal=False, arrayVal = True, array = array1)
+
+afterNumber = measureWeights(network1)
+afterDensity = afterNumber/aliveWeights
+
+simulatedBeforeNum = aliveWeights * beforeDensity
+
+
+'''array2 = array1.reshape(1, 784)
+
+for y in range (784):
+    if array2[0, y] == 1:
+        for x in range(784):
+            if network1.weights[x,y] != 0:
+                print("error")'''
+
+
